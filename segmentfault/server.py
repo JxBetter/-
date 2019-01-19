ab432660773847c6f2c3cd6bd1bd8e59ec3c5ff7
jx <33095108+JxBetter@users.noreply.github.com>
2019-01-19 17:49:03 +0800

    Add files via upload

---
+++ segmentfault/server.py
@@ -0,0 +1,36 @@
+import os
+import json
+from datetime import timedelta
+from flask import Flask, request, jsonify, send_from_directory, render_template
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/download/<file>')
+def download(file):
+	if (file == 'test_plan'):
+		alias = '思否博客测试计划.docx'
+	elif (file == 'test_examples'):
+		alias = '思否博客测试用例.xlsx'
+	elif (file == 'test_report'):
+		alias = '思否博客测试报告.docx'
+	elif (file == 'test_demand'):
+		alias = '思否博客需求文档.docx'
+	elif (file == 'auto_scripts'):
+		alias = 'scripts.rar'
+	return send_from_directory(filename=alias, directory='./', as_attachment=True)
+
+
+@app.route('/result')
+def result():
+	return render_template('res.html')
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port='9998')
